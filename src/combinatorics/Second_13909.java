2f4adc6f75b9373a23c0722b4b04c38d4b9ebb22
JunSeong_Park <ttotto0608@naver.com>
2023-08-17 18:43:17 +0900

    second 13909

--- src/combinatorics/Second_13909.java
+++ src/combinatorics/Second_13909.java
@@ -1,10 +1,10 @@
-package _baekjoon;
+package combinatorics;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Second_13909 {
 	// 입력
 	//	첫 번째 줄에는 창문의 개수와 사람의 수 N(1 ≤ N ≤ 2,100,000,000)이 주어진다.
 	// 출력
