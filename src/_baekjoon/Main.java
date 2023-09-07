1bc30e3d0eb99b50b10d9183677d45ec9954bc87
JunSeong_Park <ttotto0608@naver.com>
2023-09-07 23:42:29 +0900

    second 1463

--- src/_baekjoon/Main.java
+++ src/_baekjoon/Main.java
@@ -3,49 +3,27 @@ package _baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	// 입력
-	//  첫째 줄에 집의 수 N(2 ≤ N ≤ 1,000)이 주어진다. 
-	//  둘째 줄부터 N개의 줄에는 각 집을 빨강, 초록, 파랑으로 칠하는 비용이 1번 집부터 한 줄에 하나씩 주어진다. 
-	//  집을 칠하는 비용은 1,000보다 작거나 같은 자연수이다.
+	//  첫째 줄에 1보다 크거나 같고, 106보다 작거나 같은 정수 N이 주어진다.
 	// 출력
-	//	첫째 줄에 모든 집을 칠하는 비용의 최솟값을 출력한다.
-	static int dp[][], arr[][];
+	//	첫째 줄에 연산을 하는 횟수의 최솟값을 출력한다.
+	static int dp[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		arr = new int [n][3];
-		dp = new int [n][3];
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
-			arr[i][2] = Integer.parseInt(st.nextToken());
-		}
-		dp[0][0] = arr[0][0];
-		dp[0][1] = arr[0][1];
-		dp[0][2] = arr[0][2];
-		
-		System.out.print(Math.min(solve(n - 1, 0), Math.min(solve(n - 1, 1), solve(n - 1, 2))));
+		dp = new int [n + 1];
+		System.out.println(solve(n));
 	}
 	
 	
-	public static int solve(int n, int color) { 
-		if (dp[n][color] == 0) {
-			if (color == 0) {
-				dp[n][0] = Math.min(solve(n - 1, 1), solve(n - 1, 2)) + arr[n][0];
-			}
-			
-			else if (color == 1) {
-				dp[n][1] = Math.min(solve(n - 1, 0), solve(n - 1, 2)) + arr[n][1];
-			}
-			
-			else {
-				dp[n][2] = Math.min(solve(n - 1, 0), solve(n - 1, 1)) + arr[n][2];
-			}
+	public static int solve(int n) {
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + 1;
+			if (i % 2 == 0) dp[i] = Math.min(dp[i/2] + 1, dp[i]);
+			if (i % 3 == 0) dp[i] = Math.min(dp[i/3] + 1, dp[i]);
 		}
-		return dp[n][color];
+		return dp[n];
 	}
 }
---
+++ src/dynamic_programming/Second_1463.java
@@ -0,0 +1,5 @@
+package dynamic_programming;
+
+public class Second_1463 {
+
+}
