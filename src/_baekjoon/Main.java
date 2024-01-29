a771c5fd6d50d94a66684de90c9a87f8e81d0311
JunSeong_Park <ttotto0608@naver.com>
2024-01-29 23:34:33 +0900

    first 11054

--- src/_baekjoon/Main.java
+++ src/_baekjoon/Main.java
@@ -7,24 +7,44 @@ import java.util.StringTokenizer;
 
 public class Main {
 	// 입력
-	//	첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다.
-	//	둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
+	//	첫째 줄에 수열 A의 크기 N이 주어지고, 둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ N ≤ 1,000, 1 ≤ Ai ≤ 1,000)
 	// 출력
-	//	첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
-	static int n, arr[], dp[];
+	//	첫째 줄에 수열 A의 부분 수열 중에서 가장 긴 바이토닉 수열의 길이를 출력한다.	
+	static int N, arr[], dp_l[], dp_r[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine()); arr = new int [n]; dp = new int [n];
+		N = Integer.parseInt(br.readLine());
+		arr = new int [N]; dp_l = new int [N]; dp_r = new int [N];
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < n; i++) 
-			arr[i] = Integer.parseInt(st.nextToken());
-		int result = 0;
-		for (int i = 0; i < n; i++) {
-			dp[i] = 1;
-			for (int j = i - 1; j >= 0; j--)
-				if (arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
-			result = Math.max(result, dp[i]);
+		for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			lis(i); lds(i);
 		}
-		System.out.print(result);
+		int result = 0;
+		for (int i = 0; i < N; i++) result = Math.max(result, dp_l[i] + dp_r[i]);
+		System.out.print(result - 1);
+	}
+	
+	static int lis(int n) {
+		if (dp_l[n] == 0) {
+			dp_l[n] = 1;
+			
+			for (int i = n - 1; i >= 0; i--) {
+				if (arr[i] < arr[n]) dp_l[n] = Math.max(dp_l[n], lis(i) + 1);
+			}
+		}
+		
+		return dp_l[n];
+	}
+	
+	static int lds(int n) {
+		if (dp_r[n] == 0) {
+			dp_r[n] = 1;
+			
+			for (int i = n + 1; i < dp_r.length; i++) {
+				if (arr[i] < arr[n]) dp_r[n] = Math.max(dp_r[n], lds(i) + 1);
+			}
+		}
+		return dp_r[n];
 	}
 }
