8f1795635e3e7e8cd34bc79c903176b212b74c7a
JunSeong_Park <ttotto0608@naver.com>
2025-03-17 20:33:58 +0900

    first 1931

--- src/sort/First_1931.java
+++ src/sort/First_1931.java
@@ -1,4 +1,4 @@
-package _baekjoon;
+package sort;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
-public class Main {
+public class First_1931 {
 	// 입력
 	//	첫째 줄에 회의의 수 N(1 ≤ N ≤ 100,000)이 주어진다. 
 	//	둘째 줄부터 N+1 줄까지 각 회의의 정보가 주어지는데 이것은 공백을 사이에 두고 회의의 시작시간과 끝나는 시간이 주어진다. 
