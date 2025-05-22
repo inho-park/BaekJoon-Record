b2f54118bd61c602d284bbe571b693d594c810e8
JunSeong_Park <ttotto0608@naver.com>
2025-05-22 15:11:40 +0900

    first 11401

--- src/recursion/First_11401.java
+++ src/recursion/First_11401.java
@@ -1,15 +1,19 @@
-package _baekjoon;
+package recursion;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class First_11401 {
 	// 입력
 	//	첫째 줄에 N과 K가 주어진다. (1 ≤ N ≤ 4,000,000, 0 ≤ K ≤ N)
 	// 출력
 	//	binom{N}{K}를 1,000,000,007로 나눈 나머지를 출력한다.
+	
+	// 실패 원인 : 시간초과
+	// n, k에 대한 이항 계수를 F(n, k)라 봤을 때, F(n, k) = F(n - 1, k - 1) + F(n, k - 1)인 특성을 반영했더니, 
+	// n 과 k 가 특정 상황일 때 재귀 함수 사용이 너무 많아져서 실행속도 저하 Ex) n = 4000000, k = 2000000
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
