fa34403451a34070316e07c5c54741a35d615f51
JunSeong_Park <ttotto0608@naver.com>
2025-06-12 19:51:29 +0900

    second 11279

--- src/stack_queue_deque/Second_11279.java
+++ src/stack_queue_deque/Second_11279.java
@@ -1,4 +1,4 @@
-package _baekjoon;
+package stack_queue_deque;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-public class Main {
+public class Second_11279 {
 	// 입력
 	//	첫째 줄에 연산의 개수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 
 	//	만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고, x가 0이라면 배열에서 가장 큰 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 
