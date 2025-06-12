3d871591fdabe81b178be0f04f1882dff98bbe90
JunSeong_Park <ttotto0608@naver.com>
2025-06-12 19:50:29 +0900

    first 11279

--- src/stack_queue_deque/First_11279.java
+++ src/stack_queue_deque/First_11279.java
@@ -1,4 +1,4 @@
-package _baekjoon;
+package stack_queue_deque;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,14 +6,15 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Main {
+public class First_11279 {
 	// 입력
 	//	첫째 줄에 연산의 개수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 
 	//	만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고, x가 0이라면 배열에서 가장 큰 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 
 	//	입력되는 자연수는 2^31보다 작다.
 	// 출력
 	//	입력에서 0이 주어진 횟수만큼 답을 출력한다. 만약 배열이 비어 있는 경우인데 가장 큰 값을 출력하라고 한 경우에는 0을 출력하면 된다.
-	
+	// 실패원인
+	// 	시간 초과 (데이터를 출력할 때마다 정렬한 것이 원인)
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
