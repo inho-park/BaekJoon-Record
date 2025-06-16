e11e28f8422eb7945a8fd9e57c1fbc3b8a20082a
JunSeong_Park <ttotto0608@naver.com>
2025-06-16 17:27:08 +0900

    first 11286

--- src/stack_queue_deque/First_112886.java
+++ src/stack_queue_deque/First_112886.java
@@ -1,4 +1,4 @@
-package _baekjoon;
+package stack_queue_deque;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class Main {
+public class First_112886 {
 	// 입력
 	//	첫째 줄에 연산의 개수 N(1≤N≤100,000)이 주어진다. 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 
 	//	만약 x가 0이 아니라면 배열에 x라는 값을 넣는(추가하는) 연산이고, x가 0이라면 배열에서 절댓값이 가장 작은 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 
