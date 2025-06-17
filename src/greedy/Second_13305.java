bec771968759612325941e87e9b5154ee1311a41
JunSeong_Park <ttotto0608@naver.com>
2025-06-17 17:09:17 +0900

    second 13305

--- src/greedy/Second_13305.java
+++ src/greedy/Second_13305.java
@@ -1,11 +1,11 @@
-package _baekjoon;
+package greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Second_13305 {
 	// 입력
 	//	표준 입력으로 다음 정보가 주어진다. 첫 번째 줄에는 도시의 개수를 나타내는 정수 N(2 ≤ N ≤ 100,000)이 주어진다. 
 	//	다음 줄에는 인접한 두 도시를 연결하는 도로의 길이가 제일 왼쪽 도로부터 N-1개의 자연수로 주어진다. 
