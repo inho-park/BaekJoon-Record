1f4ebbb7a86b47ea30030cfebf7ed69c01d29433
JunSeong_Park <ttotto0608@naver.com>
2025-07-04 15:40:11 +0900

    first 2075

--- src/stack_queue_deque/First_2075.java
+++ src/stack_queue_deque/First_2075.java
@@ -1,4 +1,4 @@
-package _baekjoon;
+package stack_queue_deque;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class First_2075 {
 	// 입력
 	//	첫째 줄에 N(1 ≤ N ≤ 1,500)이 주어진다. 
 	//	다음 N개의 줄에는 각 줄마다 N개의 수가 주어진다. 표에 적힌 수는 -10억보다 크거나 같고, 10억보다 작거나 같은 정수이다.
