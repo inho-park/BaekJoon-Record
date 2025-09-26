2d3f3f52324272ab439da69e7e413e741c230f27
JunSeong_Park <ttotto0608@naver.com>
2025-09-26 14:54:11 +0900

    20250926

---
+++ src/test/First_25083.java
@@ -0,0 +1,16 @@
+package test;
+
+public class First_25083 {
+	// 입력
+	//	입력은 없다.
+	// 출력
+	//	새싹을 출력한다.
+	public static void main(String[] args)  {
+		System.out.println("         ,r\'\"7");
+		System.out.println("r`-_   ,\'  ,/");
+		System.out.println(" \\. \". L_r\'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.print("      |");
+	}
+}
